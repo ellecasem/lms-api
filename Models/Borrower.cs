b4f9a1bf7463bbda8c3b41d323a72b635fe1c21f
Jessi Sibayan <jessisibayan@gmail.com>
2023-02-14 07:46:27 +0800

    Initial template

---
+++ Models/Borrower.cs
@@ -0,0 +1,9 @@
+namespace LmsApi.Models;
+
+public class Borrower
+{
+    public long Id { get; set; }
+    public String? FirstName { get; set; }
+    public String? LastName { get; set; }
+    public String? Email { get; set; }
+}
---
+++ Models/History.cs
@@ -0,0 +1,9 @@
+namespace LmsApi.Models;
+
+public class History
+{
+    public long Id { get; set; }
+    public DateTime BorrowDate { get; set; }
+    public DateTime ReturnDate { get; set; }
+    public DateTime ActualReturnDate { get; set; }
+}
