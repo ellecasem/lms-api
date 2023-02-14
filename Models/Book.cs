839eea5c0792327476c93e6baaf738534c7dc615
Jessi Sibayan <jessisibayan@gmail.com>
2023-02-14 08:00:42 +0800

    Fixed syntax

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,11 +1,12 @@
-namespace LmsApi.Models;
-
-public class Book
+namespace LmsApi.Models
 {
-    public long Id { get; set; }
-    public String? Name { get; set; }
-    public String? Author { get; set; }
-    public String? Genre { get; set; }
-    public int Quantity { get; set; }
-    public String? ShelfLocation { get; set; }
+    public class Book
+    {
+        public long Id { get; set; }
+        public String? Name { get; set; }
+        public String? Author { get; set; }
+        public String? Genre { get; set; }
+        public int Quantity { get; set; }
+        public String? ShelfLocation { get; set; }
+    }
 }
